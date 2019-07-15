4485ddc45aaa77a3e697f904acc667db6947194f
Gwilo <44943972+Gwilo@users.noreply.github.com>
2019-07-15 15:17:35 +0200

    Create LGMRESS.py

---
+++ block/iterative/LGMRESS.py
@@ -0,0 +1,5 @@
+def lgmress(): #program does nothing as written
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear Emily.")
+    print("Happy Birthday to you!")
